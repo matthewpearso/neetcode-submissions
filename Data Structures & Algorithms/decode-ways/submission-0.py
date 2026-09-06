9b46d270f446c3f9b2f2e0feace7a8d3fdde3636
Matthew Pearson <matthewpearson803@gmail.com>
2026-09-06 14:35:09 +0200

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s) : 1}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            
+            if s[i] == '0':
+                return 0
+
+            res = dfs(i + 1)
+            
+            if int(s[i:i+2]) > 9 and int(s[i:i+2]) < 27:
+                res += dfs(i + 2)
+            
+            dp[i] = res
+            return res
+        
+        return dfs(0)
+            
